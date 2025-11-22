88583b2e3d6223b21c10cfed1b9cee7b916ac688
Sebazz <90474850+uSebazz@users.noreply.github.com>
2025-11-22 11:31:40 -0500

    Update Docusaurus config for new site settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,16 +20,17 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://usebazz.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'usebazz', // Usually your GitHub org/user name.
+  projectName: 'guide', // Usually your repo name.
+  trailingSlash: false,
+  
   onBrokenLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
