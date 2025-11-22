e87c897a16660e41efe464be75efc712d66d5e30
Sebazz <90474850+uSebazz@users.noreply.github.com>
2025-11-22 16:06:48 +0000

    feature: Details and setup ⏳

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Welcome to my guide page',
+  title: `Sebazz's guide`,
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+          Guide
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
@@ -28,11 +28,8 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    <Layout>
       <HomepageHeader />
       <main>
         <HomepageFeatures/>
