fe12f53dcd02eea9257f365a3eab98836d1a33ad
Sebazz <90474850+uSebazz@users.noreply.github.com>
2025-11-22 11:38:20 -0500

    Change baseUrl to '/guide/' in config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://usebazz.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/guide/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
